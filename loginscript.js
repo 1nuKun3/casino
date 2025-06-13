51795d0c1596fde591a797fbaaf9e76dfc79a1af
harutacasino <maikura121209@gmail.com>
2025-06-14 08:51:43 +0900

    Update loginscript.js

--- loginscript.js
+++ loginscript.js
@@ -5,7 +5,6 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
 
     if (username === 'test' && password === '1234') {
-        alert('ログイン成功！');
         window.location.href = 'casino.html'; // 次の画面へ
     } else {
         alert('ログイン失敗。ユーザー名またはパスワードを確認してください。');
