10a107483fee9ffae3eb50fed1255560769f719c
harutacasino <maikura121209@gmail.com>
2025-06-14 16:04:28 +0900

    Update loginscript.js

--- loginscript.js
+++ loginscript.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const password = document.getElementById('password').value;
 
     // GASのWebアプリURLに書き換えてください
-    const GAS_URL = 'https://script.google.com/macros/s/AKfycbxjFVMVn9utG4oDLEUKQsQkx-zU-dNwq9HZfDHO5V9SASzlk-xlzc__cMf4jB1ERFLing/exec';
+    const GAS_URL = 'https://script.google.com/macros/s/AKfycbzsLAH1pqoKZgdjWfcdnE_FBgahMQSaxUszJKB3ui3HD0nrbhqxgcAIfl_RHE7M5l8Blg/exec';
 
     // クエリパラメータを作成
     const params = new URLSearchParams({
